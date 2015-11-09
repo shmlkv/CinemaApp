4eac5dcf5246c7b11fa02ea275aac3d1fa12e0e9
splo1t <splo1t@icloud.com>
2015-11-09 21:58:54 +0200

    Inserting in DB

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require('express'),
-	pg = require('pg');
-
+var express = require('express');
+var config = require('./core/dbconfig.json');
+var mongoose = require('mongoose');
 var index = require('./routes/index');
+var pg = require('pg');
 
 var app = module.exports = express();
 
@@ -11,19 +12,34 @@ app.set('views', __dirname + '/views');
 app.use('/', index);
 app.use(express.static(process.cwd() + '/public'));
 
+db = mongoose.connection;
 
-/*app.use(express.static(__dirname + '/public'));
+db.on('error', console.error);
+db.once('open', function() {
+	Schema = mongoose.Schema;
+ var Schema = mongoose.Schema;
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+ var filmSchema = new Schema({
+   title:  String,
+   cover: String,
+   cinema:   String,
+   date: { type: Date, default: Date.now }
+ });
+ var Films = mongoose.model('Films', filmSchema);
+ var f = new Films({
+ 	title: 'FILMNAME'
+ });
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});*/
+ f.save(function (err) {
+   if (err) return handleError(err);
+   Films.findById(f, function (err, doc) {
+     if (err) return handleError("!" + err);
+     console.log(doc); // { name: 'mongodb.org', _id: '50341373e894ad16347efe12' }
+   })
+ })
+});
+console.log(config.url);
+mongoose.connect(config.url2);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('app is runned');
