575fb787aa7f240c3e0cc7c618284fed5eed4fb7
splo1t <splo1t@icloud.com>
2015-11-10 21:04:52 +0200

    Output of data from DB

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var config = require('./core/dbconfig.json');
-var mongoose = require('mongoose');
 var index = require('./routes/index');
-var pg = require('pg');
-
 var app = module.exports = express();
+
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -12,34 +9,6 @@ app.set('views', __dirname + '/views');
 app.use('/', index);
 app.use(express.static(process.cwd() + '/public'));
 
-db = mongoose.connection;
-
-db.on('error', console.error);
-db.once('open', function() {
-	Schema = mongoose.Schema;
- var Schema = mongoose.Schema;
-
- var filmSchema = new Schema({
-   title:  String,
-   cover: String,
-   cinema:   String,
-   date: { type: Date, default: Date.now }
- });
- var Films = mongoose.model('Films', filmSchema);
- var f = new Films({
- 	title: 'FILMNAME'
- });
-
- f.save(function (err) {
-   if (err) return handleError(err);
-   Films.findById(f, function (err, doc) {
-     if (err) return handleError("!" + err);
-     console.log(doc); // { name: 'mongodb.org', _id: '50341373e894ad16347efe12' }
-   })
- })
-});
-console.log(config.url);
-mongoose.connect(config.url2);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('app is runned');
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,50 @@
-var express = require('express');
-var app = module.exports = express();
+var express = require('express'),
+	app = module.exports = express();
+var config = require('../core/dbconfig.json');
 
+var mongoose = require('mongoose');
+mongoose.connect(config.db_url);
+var db = mongoose.connection;
 
-app.get('/', function(req, res){
-  res.render('home', {word: 'WORLD'});
+var getTitles = false,
+	title_arr = [];
+
+db.on('error', console.error);
+
+app.get('/', function(req, res, next){
+	var response = function () {
+		if (getTitles) {
+			res.render('home', {
+				title: 'Recent games',
+				word: 'WORLD',
+				arr : title_arr
+			});
+		}
+		console.log(title_arr);
+	};
+	db.once('open', function() {
+		var Schema = mongoose.Schema;
+
+		var filmSchema = new Schema({
+			title:  String,
+			cover_url: String,
+			desc: String,
+			genre: Array,
+			//release_date: { type: Date("<dd.mm.YYYY>")}
+		});
+
+		var films = mongoose.model('Films', filmSchema);
+		
+		films.find(function (err, films) {
+			if (err) return console.error(err);
+				films.forEach(function(film, index, array) {
+					title_arr.push(film.title);
+					if (index == films.length-1){
+						getTitles = true;
+						response();
+					}
+				});
+		});
+	});
 });
+
