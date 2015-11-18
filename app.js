87a41af8821339b6489ea9829117f7cc8815f10a
splo1t <splo1t@icloud.com>
2015-11-18 20:40:21 +0200

    Calendar for period, new page for it, Momentjs for  date format,

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var index = require('./routes/index');
 var app = module.exports = express();
 
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.use('/', index);
+app.use('/', require('./routes/index'));
+
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(function(err) {console.error(err);});
@@ -14,3 +14,4 @@ app.use(function(err) {console.error(err);});
 app.listen(process.env.PORT || 3000, function(){
   console.log('app is runned');
 });
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = module.exports = express();
 var config = require('../core/config.json');
-
+var moment = require('moment');
 
 var mongoose = require('mongoose');
 mongoose.connect(config.db_url);
@@ -30,35 +30,49 @@ var films = mongoose.model('Films', filmSchema);
 var cinemas = mongoose.model('Cinemas', cinemasSchema);
 var sessions = mongoose.model('Sessions', sessionsSchema);
 
+
 db.on('error', console.error);
 
 app.get('/', function(req, res){
     films.find({}).select('title').exec(function(err, data_films){
         if(!err ){
             cinemas.find({}).select('city').exec(function(err, data_cities){
-
-                res.render('home', {
-                    title: 'home',
-                    films : data_films,
-                    cities: data_cities
-                });
-                console.log("Rendered page " + req.originalUrl)
+                if(!err ) {
+                    sessions.find({}).select('date').exec(function (err, data_sessions) {
+                        if(!err ) {
+                            res.render('home', {
+                                title: 'home',
+                                films: data_films,
+                                cities: data_cities,
+                                dates: data_sessions
+                            });
+                            console.log("Rendered page " + req.originalUrl)
+                        }
+                    });
+                }
             });
 
         }
     });
-
 });
 
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.post('/period', urlencodedParser, function (req, res) {
+    date_from = moment(req.body.date_from, 'DD-MM-YYYY').format('YYYY-MM-DD[T]HH:mm:ss[Z]');
+    date_to =  moment(req.body.date_to, 'DD-MM-YYYY').format('YYYY-MM-DD[T]HH:mm:ss[Z]');
+    res.send(date_from + " " + date_to);
+    console.log(req.body);
+    console.log("Rendered page " + req.originalUrl)
 
-
-//var f = new Cinemas({
+});
+//var f = new cinemas({
 //    company: 'Cinema-city',
 //    city: 'ראשון לציון',
 //    address: 'ילדי טהרן 3'
 //});
 
-// var j = new Films({
+// var j = new films({
 // 	title: 'Star Wars: Episode VII - The Force Awakens',
 // 	cover_url: 'http://ia.media-imdb.com/images/M/MV5BMTkwNzAwNDA4N15BMl5BanBnXkFtZTgwMTA2MDcwNzE@._V1_SX214_AL_.jpg',
 // 	desc: 'המשך לסדרת הסרטים הקלאסית של גורג לוקאס, המתרחש 30 שנה לאחר עלילות הפרק השישי בסדרה מ-1983',
@@ -66,9 +80,15 @@ app.get('/', function(req, res){
 // 	//release_date: { type: Date }
 // });
 
+//var j = new sessions({
+//    film_id: 313123,
+//    cinema_id: 3123321,
+//    date: new Date().toISOString()
+//});
+
 //j.save(function (err) {
 //    if (err) return console.log(err);
-//    Films.findById(f, function (err, doc) {
+//    sessions.findById(j, function (err, doc) {
 //        if (err) return console.log("!" + err);
 //        console.log(doc); // { name: 'mongodb.org', _id: '50341373e894ad16347efe12' }
 //    })
