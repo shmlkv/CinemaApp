175a0d2f3f2c508f3cd88f177f262d1a38fdbbaa
splo1t <splo1t@icloud.com>
2015-11-25 19:01:04 +0200

    Output of selected film

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -1,3 +1,7 @@
-function filmDetals(film) {
-    document.getElementById("info").innerHTML = '<img src="' + film +  '" >';
+function filmDetals(title, id, cover_url,genre,desc) {
+    document.getElementById("img").innerHTML = '<img src="' + cover_url +  '" >';
+    document.getElementById("title").innerHTML =  title;
+    document.getElementById("_id").innerHTML =  id;
+    document.getElementById("genre").innerHTML =  genre;
+    document.getElementById("desc").innerHTML =  desc;
 }
