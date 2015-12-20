4456b8b17b595d1cf353d442c14125f62cb46db4
splo1t <splo1t@icloud.com>
2015-12-20 18:43:43 +0200

    Many small fixes + API OVERVIEW

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -5,3 +5,11 @@ function filmDetals(title, id, cover_url,genre,desc) {
     document.getElementById("genre").innerHTML =  'Genres: ' + genre;
     document.getElementById("desc").innerHTML =  'Description:  ' + desc;
 }
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: false,
+        infinite: false,
+        variableWidth: true
+});
+});
